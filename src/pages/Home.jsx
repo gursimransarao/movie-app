dd3eda08c1fe2c0e5ab9f001ec26d3917baedf1c
gursimran sarao <gursimransarao@gmail.com>
2025-04-11 00:21:26 +0530

    basic routing and styling

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+import "../css/Home.css";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "Prestige", release_date: "2020" },
+    { id: 2, title: "Baby Driver", release_date: "2018" },
+    { id: 3, title: "Knives Out", release_date: "2019" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("----");
+  };
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map(
+          (movie) => (
+            //     movie.title.toLowerCase().startsWith(searchQuery) && (
+            <MovieCard movie={movie} key={movie.id} />
+          )
+          // )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
